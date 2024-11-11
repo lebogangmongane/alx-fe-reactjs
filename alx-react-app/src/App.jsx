ca08c4f3b0a0d674bfa778e328f7eb12e3863349
lebogangmongane <jerrymongane@gmail.com>
2024-11-11 05:11:55 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+        </div>
+    );
+}
+
+export default App;
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
