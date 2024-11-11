66fe51cf6b2b1f05ea66a0096a9cdec3e28e76e2
Lebogang <jerrymongane@gmail.com>
2024-11-11 19:48:42 +0200

    MAINCONTENT CHANGES

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
         <main style={{ padding: '20px', backgroundColor: '#f5f5f5', textAlign: 'center' }}>
-            <p style={{ fontSize: '18px', color: '#555', lineHeight: '1.6' }}> love to visit New York, Paris, and Tokyo.</p>
+            <p style={{ fontSize: '18px', color: '#555', lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
