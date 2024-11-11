1ae3bd4ba183756b8293af01a03f906a1180d3e7
Lebogang <jerrymongane@gmail.com>
2024-11-11 21:12:09 +0200

    task 2 commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,11 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
-import Counter from './components/Counter';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -41,9 +39,7 @@ function App() {
             <MainContent />
             <WelcomeMessage />
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <Counter />
             <Footer />
-        
         </div>
     </>
   )
