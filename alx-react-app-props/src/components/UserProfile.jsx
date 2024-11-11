5776f201c1876536a0ec9ec8ddd9cb4c0d4e6029
Lebogang <jerrymongane@gmail.com>
2024-11-11 23:40:06 +0200

    userprofile changed

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,17 @@
-const UserProfile = (props) => {
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+    // Access user data directly from context
+    const userData = useContext(UserContext);
+
     return (
-   
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <p>Name: {userData.name}</p>
+            <p>Email: {userData.email}</p>
         </div>
     );
 }
 
-export default UserProfile;
+export default UserDetails;
