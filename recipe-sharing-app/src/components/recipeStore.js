cb1576adbe840a17260e8c046f3571c24a33b37d
Lebogang <jerrymongane@gmail.com>
2024-11-13 16:34:12 +0200

    recipestore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,14 +1,39 @@
+/* eslint-disable no-unused-vars */
+// src/components/recipeStore.js
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  setRecipes: (recipes) => set({ recipes }),
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (recipeId) => set(state => ({
-    recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
+  searchTerm: '',
+  filteredRecipes: [],
+
+  // Action to set the search term
+  setSearchTerm: (term) => set(state => {
+    return { searchTerm: term };
+  }),
+
+  // Action to filter recipes based on the search term
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
   })),
+
+  // Other actions (addRecipe, deleteRecipe, updateRecipe) if required
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId),
+    filteredRecipes: state.filteredRecipes.filter(recipe => recipe.id !== recipeId)
+  })),
+
   updateRecipe: (updatedRecipe) => set(state => ({
     recipes: state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+    filteredRecipes: state.filteredRecipes.map(recipe =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
