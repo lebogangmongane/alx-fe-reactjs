f1d014965899f972b54e1fcc0ee8d642982c95d6
lebogangmongane <jerrymongane@gmail.com>
2024-11-14 18:31:26 +0200

    final changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,50 +1,25 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import RecipeList from './RecipeList';
-import AddRecipeForm from './AddRecipeForm';
-import RecipeDetails from './RecipeDetails';
-import EditRecipeForm from './components/EditRecipeForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Router>
+const App = () => (
+  <Router>
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <FavoritesList />
+      <RecommendationsList />
       <Routes>
         <Route path="/" element={<RecipeList />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-        <Route path="/edit/:id" element={<EditRecipeForm />} />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
       </Routes>
-    </Router>
-    </>
-  )
-}
+    </div>
+  </Router>
+);
 
-export default App
+export default App;
