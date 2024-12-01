30e5d5960ae0f7428fb9ba6ff7d51ae089216f18
Lebogang <jerrymongane@gmail.com>
2024-12-01 04:08:45 +0200

    Completed React with Tailwind CSS project tasks

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: { extend: {} },
+  plugins: [],
+};
